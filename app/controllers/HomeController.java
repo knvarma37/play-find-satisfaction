29b6f37386c2233b35ac3667ba724cb1611a30be
Nagendra Varma Khandavalli <nkhandavalli@soct.com>
2017-07-03 15:57:07 +0530

    Inital code for play framework

---
+++ app/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import play.mvc.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class HomeController extends Controller {
+
+    public Result index() {
+        return ok(views.html.index.render());
+    }
+    
+    public Result findSatisfaction(Integer timelimit) {
+        return ok(String.valueOf(timelimit * 100));
+    }
+
+}
